1a707d878c935c91d1ab6c52fce98fad3cf0fc05
amalkrishna007 <81072934+amalkrishna007@users.noreply.github.com>
2022-01-25 22:27:48 +0530

    Update pyrogramEngine.py

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -82,7 +82,7 @@ class TgUploader:
                 if file_.upper().endswith(VIDEO_SUFFIXES):
                     duration = get_media_info(up_path)[0]
                     if thumb is None:
-                        thumb = f"https://te.legra.ph/file/9ee45440849d434aa5512.jpg"
+                        thumb = "https://te.legra.ph/file/9ee45440849d434aa5512.jpg"
                         if self.__is_cancelled:
                             if self.__thumb is None and thumb is not None and ospath.lexists(thumb):
                                 osremove(thumb)
