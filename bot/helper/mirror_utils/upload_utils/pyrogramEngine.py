a095fa0e32b31e95ea767c6f2a848a60a9450465
amalkrishna007 <81072934+amalkrishna007@users.noreply.github.com>
2022-02-02 20:59:19 +0530

    Update pyrogramEngine.py

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -67,7 +67,7 @@ class TgUploader:
         else:
             self.__sent_msg = app.get_messages(self.__sent_msg.chat.id, self.__sent_msg.message_id)
         if CUSTOM_FILENAME is not None:
-            cap_mono = f"<code>{file_}</code>"
+            cap_mono = f"<code>@SearchITRobot.{file_}</code>"
             file_ = f"{CUSTOM_FILENAME}.{file_}"
             new_path = ospath.join(dirpath, file_)
             osrename(up_path, new_path)
